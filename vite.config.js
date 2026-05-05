3c25dbeb961edbdff7e3236eef2c321fcfb7f0de
Patricia <patrialvcas@gmail.com>
2026-05-05 10:12:54 +0200

    Redirigido a /indumentaria en vite

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// En Tailwind v4 (@tailwindcss/vite), los plugins NO van aquí.
-// Van directamente en tu archivo .css usando @plugin.
 export default defineConfig({
+  base: '/indumentaria/', 
   plugins: [
     react(),
     tailwindcss(),
